17c357103dc8d5568370a751e4c05edd1e101403
angelng1412 <ang7@stuy.edu>
2017-10-11 14:28:27 -0400

    added flashed messages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, flash
 import os 
 
 app = Flask(__name__)
@@ -16,24 +16,28 @@ def root():
 
 @app.route('/verify', methods = ['POST','GET'])
 def verify():
-    if request.form['user'] == username and request.form['pass'] == password:
-        session['user'] = request.form['user']
-        return redirect( url_for('welcome') )
-        #return render_template('welcome.html', user = request.form['user'])
+    userIn = request.form['user']
+    passIn = request.form['pass']
+    if userIn == username:
+        if passIn == password:
+            session['user'] = request.form['user']
+            flash('Login Successful')
+            return redirect( url_for('welcome') )
+        else:
+            flash('Incorrect password. Please try again.')
+            return redirect( url_for('root') )
     else:
-        return redirect( url_for('error') )
+        flash('Incorrect username. Please try again')
+        return redirect( url_for('root') )
     
 @app.route('/welcome', methods = ['POST', 'GET'])
 def welcome():
     return render_template('welcome.html', user = session['user'])
 
-@app.route('/error')
-def error():
-    return render_template('error.html')
-
 @app.route('/logout', methods = ['POST', 'GET'])
 def logout():
     session.pop('user')
+    flash('Logged Out')
     return redirect( url_for('root') )
 
 if __name__ == '__main__':
